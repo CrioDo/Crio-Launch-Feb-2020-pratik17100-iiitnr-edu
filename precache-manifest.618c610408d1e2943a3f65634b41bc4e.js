142b2d0a66c426d766fc08e4cb02463221343efc
CrioDo <sridherj@criodo.com>
2020-04-03 19:12:00 +0000

    Updates

---
+++ precache-manifest.618c610408d1e2943a3f65634b41bc4e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7226b9bcccfcedbb6bc83c9c44b0956e",
+    "url": "/Crio-Launch-Feb-2020-pratik17100-iiitnr-edu/index.html"
+  },
+  {
+    "revision": "72931bc83661ad6f35d6",
+    "url": "/Crio-Launch-Feb-2020-pratik17100-iiitnr-edu/static/css/2.57ddf7ad.chunk.css"
+  },
+  {
+    "revision": "51c3c2d70899718cf75f",
+    "url": "/Crio-Launch-Feb-2020-pratik17100-iiitnr-edu/static/css/main.b1dc9d43.chunk.css"
+  },
+  {
+    "revision": "72931bc83661ad6f35d6",
+    "url": "/Crio-Launch-Feb-2020-pratik17100-iiitnr-edu/static/js/2.25e81f3a.chunk.js"
+  },
+  {
+    "revision": "5727441246ffa49345a3574f984c2034",
+    "url": "/Crio-Launch-Feb-2020-pratik17100-iiitnr-edu/static/js/2.25e81f3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51c3c2d70899718cf75f",
+    "url": "/Crio-Launch-Feb-2020-pratik17100-iiitnr-edu/static/js/main.4ee2eb08.chunk.js"
+  },
+  {
+    "revision": "530ed8cd6f0db7635b1f",
+    "url": "/Crio-Launch-Feb-2020-pratik17100-iiitnr-edu/static/js/runtime-main.9b523857.js"
+  },
+  {
+    "revision": "c9536e0497f939475da3110091e1a10d",
+    "url": "/Crio-Launch-Feb-2020-pratik17100-iiitnr-edu/static/media/crio-logo-white.c9536e04.svg"
+  },
+  {
+    "revision": "63be71d01edef2dcf3cafba5e52553a4",
+    "url": "/Crio-Launch-Feb-2020-pratik17100-iiitnr-edu/static/media/crio-logo.63be71d0.svg"
+  }
+]);
